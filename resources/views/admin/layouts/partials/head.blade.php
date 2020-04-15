42dd54e7e1ba6a21134daa588860fe8f9a7bd2fc
MyFRA <myfragithub@gmail.com>
2020-04-15 14:31:13 +0700

    update title

--- resources/views/admin/layouts/partials/head.blade.php
+++ resources/views/admin/layouts/partials/head.blade.php
@@ -20,7 +20,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>
-    {{ $title }}
+    MyPenjualan | {{ $title }}
   </title>
   <!-- Favicon -->
   <link href="{{ asset('/admin-template/img/brand/favicon.png') }}" rel="icon" type="image/png">
